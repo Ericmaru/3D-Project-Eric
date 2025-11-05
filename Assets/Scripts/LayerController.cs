e1169be627607a95e4781e19491374dee2b09687
Ericmaru <ericmonsan06@gmail.com>
2025-11-05 13:27:16 +0100

    dante

---
+++ Assets/Scripts/LayerController.cs
@@ -0,0 +1,139 @@
+using System.Data.Common;
+using TreeEditor;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LayerController : MonoBehaviour
+{
+    //Components
+    private CharacterController _controller;
+
+    //Inputs
+    private InputAction _moveAction;
+    private Vector2 _moveInput;
+    private InputAction _jumpAction;
+    private InputAction _lookAction;
+    private Vector2 _lookInput;
+    private InputAction _aimAction;
+
+    //Floats
+    [SerializeField] private float _movementSpeed = 5;
+    [SerializeField] private float _jumpHeight = 2;
+    [SerializeField] private float _smoothTime = 0.2f;
+    private float _turnSmoothVelocity;
+
+    //Gravity
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
+
+    //GroundSensor
+    [SerializeField] Transform _sensor;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] float _sensorRadius;
+
+    public Transform _mainCamera;
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _moveAction = InputSystem.actions["Move"];
+        _jumpAction = InputSystem.actions["Jump"];
+        _lookAction = InputSystem.actions["Look"];
+        _aimAction = InputSystem.actions["Aim"];
+        _mainCamera = Camera.main.transform;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+        _lookInput = _lookAction.ReadValue<Vector2>();
+
+        if (_aimAction.IsInProgress())
+        {
+            AimMovement();
+        }
+        else
+        {
+            Movement();
+        }
+
+        if (_jumpAction.WasPressedThisFrame() && IsGrounded())
+        {
+            Jump();
+        }
+        Gravity();
+
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            _controller.Move(moveDirection.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void AimMovement()
+    {
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _mainCamera.eulerAngles.y, ref _turnSmoothVelocity, _smoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            _controller.Move(moveDirection.normalized * _movementSpeed * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+        _controller.Move(_playerGravity * Time.deltaTime);
+    }
+
+    void Gravity()
+    {
+        if (!IsGrounded())
+        {
+            _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if (IsGrounded() && _playerGravity.y < 0)
+        {
+            _playerGravity.y = 0;
+        }
+        _controller.Move(_playerGravity * Time.deltaTime);
+
+    }
+
+    bool IsGrounded()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(_sensor.position, -transform.up, out hit, _sensorRadius, _groundLayer))
+        {
+            Debug.DrawRay(_sensor.position, -transform.up * _sensorRadius, Color.red);
+            return true;
+        }
+        else
+        {
+            Debug.DrawRay(_sensor.position, -transform.up * _sensorRadius, Color.green);
+            return false; 
+        }
+    }
+
+}   
